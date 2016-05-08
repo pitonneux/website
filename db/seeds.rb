f5e436c745286fbcb0a7295d0014a98a20e427b6
Kira McLean <kiraemclean@gmail.com>
2016-05-08 10:53:29 -0400

    add organizations to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Organization.delete_all
 
 Organization.create! [
-  { name: 'We Work', description: "Create Your Life's Work" }
+  { name: 'We Work',
+    description: "## Create Your Life's Work\r\nWe're proud to be sponsored by WeWork, who are generously providing our office location and venue for our workshops at WeWork Place Ville Marie.",
+    remote_logo_url: "http://res.cloudinary.com/pitonneux-com/image/upload/v1462717320/yedxz9ombf6g8poyzsj4.png"
+  },
+  { name: 'Topolino',
+    description: "## Delicious Food for Thoughts\r\nCatering during the workshops is provided by our wonderful friends at topolino! Enjoy fresh, seasonal meals prepared by budding chefs eager to make you discover specialties from all around the world.",
+    remote_logo_url: "http://res.cloudinary.com/pitonneux-com/image/upload/v1462714773/eyk1uu51wrdaizh5by8m.png"
+  }
 ]
+
+
