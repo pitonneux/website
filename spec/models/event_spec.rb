a22c8d491b7e39fa0b11e29c7e70b97da668627e
Kira McLean <kiraemclean@gmail.com>
2016-06-18 14:52:56 -0400

    test event scopes

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,4 +3,25 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
   it { is_expected.to belong_to :location }
   it { is_expected.to validate_presence_of :name }
+
+  describe 'featured scope' do
+    let(:featured)     { create :event, featured: true  }
+    let(:not_featured) { create :event, featured: false }
+
+    subject { Event.featured }
+
+    it { is_expected.to include featured }
+    it { is_expected.not_to include not_featured }
+  end
+
+  describe 'upcoming scope' do
+    let(:upcoming)     { create :event, starts_at: 1.month.from_now }
+    let(:not_upcoming) { create :event, starts_at: 1.month.ago      }
+
+    subject { Event.upcoming }
+
+    it { is_expected.to include upcoming }
+    it { is_expected.not_to include not_upcoming }
+
+  end
 end
