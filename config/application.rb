537811a83e2b7c466a46a86016eef3c07e45d680
Kira McLean <kiraemclean@gmail.com>
2016-06-19 13:52:39 -0400

    fix deprecation warnings

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,6 @@ module Pitonneux
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.active_record.raise_in_transactional_callbacks = true
     config.action_controller.action_on_unpermitted_parameters = :raise
   end
 end
