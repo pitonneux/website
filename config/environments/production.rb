bb38ead4e8e4499fc9755cbd0237810bb26e8483
Kira McLean <kiraemclean@gmail.com>
2016-07-08 15:56:39 -0400

    prod environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,10 +13,12 @@ Rails.application.configure do
   config.log_level = :debug
   config.log_tags = [:request_id]
 
-  config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default charset: 'utf-8'
   config.action_mailer.default_url_options = { host: 'pitonneux.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.smtp_settings = {
     address:        'smtp.sendgrid.net',
