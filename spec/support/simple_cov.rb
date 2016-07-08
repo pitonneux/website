91f323d73024b351f1ecfc80117492ae6fa11c6d
Kira McLean <kiraemclean@gmail.com>
2016-07-08 07:56:05 -0400

    try to make coveralls work

--- spec/support/simple_cov.rb
+++ spec/support/simple_cov.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
-require 'simplecov'
+# require 'simplecov'
 require 'coveralls'
 
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+# SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 
 # save to CircleCI's artifacts directory if we're on CircleCI
 if ENV['CIRCLE_ARTIFACTS']
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
   SimpleCov.coverage_dir(dir)
 end
+#
+# SimpleCov.start do
+#   add_filter 'app/secrets'
+# end
 
-SimpleCov.start do
-  add_filter 'app/secrets'
-end
+Coveralls.wear!('rails')
