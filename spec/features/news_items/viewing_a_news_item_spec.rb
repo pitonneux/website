ebb211f7a34b5d4bd4495204f9562053f244d05f
Kira McLean <kiraemclean@gmail.com>
2016-08-13 13:50:33 -0400

    allow anyone to see news items

---
+++ spec/features/news_items/viewing_a_news_item_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature 'viewing news items' do
+  scenario 'it is available for guests' do
+    news_item = create :news_item
+
+    visit news_item_path(news_item)
+    expect(page).to have_content news_item.title
+    expect(page).to have_content news_item.description
+  end
+end
