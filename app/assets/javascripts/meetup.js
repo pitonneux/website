1dfb6ab25002fe42f475164a3fe03edb1c0b6fa3
Denise L'Hirondelle <denise.lhirondelle@gmail.com>
2016-08-23 14:23:43 -0400

    #27 new script can now access meetup event info

--- app/assets/javascripts/meetup.js
+++ app/assets/javascripts/meetup.js
@@ -1,57 +1,18 @@
+
 $(document).ready(function() {
-  function createCORSRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-
-    if ("withCredentials" in xhr) {
-
-      // Check if the XMLHttpRequest object has a "withCredentials" property.
-      // "withCredentials" only exists on XMLHTTPRequest2 objects.
-      xhr.open(method, url, true);
-
-    } else if (typeof XDomainRequest != "undefined") {
-
-      // Otherwise, check if XDomainRequest.
-      // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-      xhr = new XDomainRequest();
-      xhr.open(method, url);
-
-    } else {
-
-      // Otherwise, CORS is not supported by the browser.
-      xhr = null;
-
-    }
-    return xhr;
-  }
-
-  function getApiData() {
-    //TODO: Why is xhr empty?
-
-    //var url = 'https://api.meetup.com/pitonneux/events'; //production
-    var url = 'https://api.meetup.com/2/events?key=ABDE12456AB2324445&group_urlname=ny-tech&sign=true'; //test
-
-    var xhr = createCORSRequest('GET', url);
-
-    if (!xhr) {
-      throw new Error('CORS not supported');
+    function addScript(src) {
+        console.log(src);
+        var s = document.createElement( 'script' );
+        s.setAttribute( 'src', src );
+        document.body.appendChild( s );
     }
 
-    xhr.onload = function() {
-      var allTheData = xhr.getResponseText;
-      console.log("At least it loaded");
-      return allTheData;
-    };
+    window.gotIt = function(data) {
+        alert(data.data[0].created);
+        $('#results').append('Got... something');
+    }
 
-    xhr.onerror = function() {
-      //throw new Error('Woops, there was an error making the request.');
-      throw new Error(xhr.statusText);
-    };
-
-    xhr.send();
-  }
-
-  var eventsList = getApiData();
-
-  //TODO: Do stuff with data
+    addScript('https://api.meetup.com/pitonneux/events?photo-host=public&page=10&sig_id=195641644&sig=3c9a7b49b18362a2e7f49adb63e161d4f6da4ec3&callback=gotIt');
 
 });
+
